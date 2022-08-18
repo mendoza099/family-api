e63a7a8069df9504398fa99c30e76784717e5f34
mendoza099 <sergiomendozadominguez@gmail.com>
2022-08-18 16:40:34 +0000

    fin

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
+import os, json
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
+
 #from models import Person
 
 app = Flask(__name__)
@@ -14,6 +12,23 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+jackson_family.add_member({
+"first_name":"John",
+"id": 1,
+"lucky_numbers":[7, 13, 22]
+})
+jackson_family.add_member({
+"first_name":"Jane",
+"id":2,
+"lucky_numbers":[10, 14, 3]
+})
+jackson_family.add_member({
+"first_name":"Jimmy",
+"id":3,
+"lucky_numbers":[1]
+})
+
+
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -26,19 +41,40 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def handle_all():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
-
-    return jsonify(response_body), 200
-
+@app.route('/member/<int:id>', methods=['GET'])
+def handle_get_one(id):
+    member = jackson_family.get_member(id)
+    if not member:
+        return jsonify({}), 404
+    
+    return jsonify(member), 200
 # this only runs if `$ python src/app.py` is executed
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def handle_delete_one(id):
+    member = jackson_family.delete_member(id)
+    if member:
+        return jsonify({
+            'done': True
+        }), 200     
+    
+    return jsonify({}), 404
+     # hay que hacer el request body como el ejercicio poniendo los datos del diccionario de cada miembro de la familia
+@app.route('/member', methods=['POST'])
+def handle_post_one():
+    # members = jackson_family.get_all_members(), no es necesario por la función add_members. Tampoco se necesita parámetros porque pasamos el body por la funcion y no por la url
+    request_body = request.data
+    member = json.loads(request_body)
+    jackson_family.add_member(member)
+
+    return jsonify({}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -18,18 +18,26 @@ class FamilyStructure:
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
-
+        
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member['last_name'] = self.last_name
 
+        if 'id' not in member:
+            member['id'] = self._generateId()
+
+        self._members.append(member)
+        
+ 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        miembro = self.get_member(id)
+        if miembro: #"super pythonic" descriptivo, esto significa que if miembro is NOT None "tira pa' lante" y devuelve el diccionario llamado extrañamente "miembro"
+            self._members.remove(miembro)
+            return miembro
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for miembro in self._members:
+            if miembro['id'] == id: #aquí ya tenemos un if, de que si nos devuelve un id que existe nos devuelve un diccionario de la lista, no devuelve nada
+                return miembro       
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
